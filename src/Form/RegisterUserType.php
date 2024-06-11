cdbde37d9af7e149036b9c61e7e2b9c2c06a587e
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-06-11 21:44:50 +0200

    creation 1er formulaire

---
+++ src/Form/RegisterUserType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('firstname')
+        ->add('lastname')
+            ->add('email')
+            // ->add('roles')
+            ->add('password')
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire",
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
