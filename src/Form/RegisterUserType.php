bb61fea395bcb214429f92fc2116e925c3d225be
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-06-12 05:52:57 +0200

    affichage formulaire

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,13 +16,36 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('firstname')
-        ->add('lastname')
-            ->add('email')
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre prénom',
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre nom',
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre adresse email',
+                'attr' => [
+                    'placeholder' => 'Entrez votre adresse email',
+                ],
+            ])
             // ->add('roles')
-            ->add('password')
+            ->add('password', PasswordType::class, [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'Choisissez votre mot de passe',
+                ],
+            ])
             ->add('submit', SubmitType::class, [
-                'label' => "S'inscrire",
+                'label' => "M'inscrire",
+                'attr' => [
+                    'class' => 'btn btn-dark w-100 mt-2',
+                ],
             ])
         ;
     }
