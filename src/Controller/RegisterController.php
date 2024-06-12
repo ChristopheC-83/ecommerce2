6e69eb39d70002cf2f093c702ffc0ea0f0a387a8
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-06-12 17:42:22 +0200

    recup données formulaire

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\RegisterUserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Expr\Andx;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+
+    //  on appelle une injection de dépendance, index fonctionnera avec Request utilisable dans une variable, ici, $request
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $form =$this->createForm(RegisterUserType::class);
+        $user = new User();
+
+        // dd($request);
+
+        $form = $this->createForm(RegisterUserType::class, $user);  // les infos $form->getData() iront dans l'objet $user
+
+        // Si formulaire soumis en écoutant la request
+        $form->handleRequest($request);
+        // si le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // dd($form->getData());
+
+            // enregistres les datas et DB, injection de dépendance avec EntityManagerInterface
+            $entityManager->persist($user);   // fige les données
+            $entityManager->flush();          // envoi les données
+
+
+        }
+        // potentiellement, envoi un message de confirmation
 
         return $this->render('register/index.html.twig', [
             'registerForm' => $form->createView(),
